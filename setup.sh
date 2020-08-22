574fbfd0dc58e0df46853e9e8438f3a1aeefb8a1
espylapiza <39075622+Espylapiza@users.noreply.github.com>
2020-08-23 03:17:44 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,12 @@
 # or replace it with gist url
 url="https://raw.githubusercontent.com/Espylapiza/config/master"
 
+sudo add-apt-repository ppa:x4121/ripgrep
 sudo apt update
 sudo apt install -y curl vim zsh ripgrep
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth 1 https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
