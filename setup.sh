eb6cf1228e9d6f8487163b7bbeb11153acd64f4a
espylapiza <39075622+Espylapiza@users.noreply.github.com>
2020-08-23 03:10:12 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,12 @@
 url="https://raw.githubusercontent.com/Espylapiza/config/master"
 
 sudo apt update
-sudo apt install -y curl vim zsh fzf ripgrep
+sudo apt install -y curl vim zsh ripgrep
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 
 sudo sed -i -e "s_/bin/bash_/bin/zsh_" /etc/passwd
 
