52baeff5c0cfc6afa9cc614366cae4e4266562ed
espylapiza <39075622+Espylapiza@users.noreply.github.com>
2020-08-23 01:25:53 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 sudo apt install zsh
 sudo sed -i -e "s_/bin/bash_/bin/zsh_" /etc/passwd
 
-gist_url="https://gist.githubusercontent.com/Espylapiza/e2d3143a7d6e48f122447134261c865e/raw/b345b0a2f0a76ef601f66be0d60efc2cad48c2a6"
+# or replace it with gist url
+url="https://raw.githubusercontent.com/Espylapiza/config/master"
 
-wget "$gist_url/.vimrc" -O ~/.vimrc
-wget "$gist_url/.tmux.conf" -O ~/.tmux.conf
+wget "$url/.vimrc" -O ~/.vimrc
+wget "$url/.tmux.conf" -O ~/.tmux.conf
