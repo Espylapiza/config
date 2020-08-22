84ddca10082a252e5012a6cac4e6d331e2acc1ee
espylapiza <39075622+Espylapiza@users.noreply.github.com>
2020-08-23 03:02:31 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-sudo apt install curl zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-sudo sed -i -e "s_/bin/bash_/bin/zsh_" /etc/passwd
 
 # or replace it with gist url
 url="https://raw.githubusercontent.com/Espylapiza/config/master"
 
+sudo apt update
+sudo apt install -y curl vim zsh fzf ripgrep
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+sudo sed -i -e "s_/bin/bash_/bin/zsh_" /etc/passwd
+
 wget "$url/.vimrc" -O ~/.vimrc
 wget "$url/.tmux.conf" -O ~/.tmux.conf
+wget "$url/.zshrc" -O ~/.zshrc
