80c1e2be3e73429417494903f9e4aa2bc3e03b2b
espylapiza <39075622+Espylapiza@users.noreply.github.com>
2020-08-23 03:34:38 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,15 +3,16 @@
 # or replace it with gist url
 url="https://raw.githubusercontent.com/Espylapiza/config/master"
 
-sudo add-apt-repository ppa:x4121/ripgrep -y
+sudo add-apt-repository -y ppa:x4121/ripgrep
 sudo apt update
 sudo apt install -y curl vim zsh ripgrep
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth 1 https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+~/.fzf/install --all
 
+sudo cp /etc/passwd /etc/passwd.bak
 sudo sed -i -e "s_/bin/bash_/bin/zsh_" /etc/passwd
 
 mv ~/.vimrc ~/.vimrc.bak
